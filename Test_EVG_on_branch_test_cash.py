d954f813182f67ec5c93984ef1612edb5d507b46
nN1ce <nicevector@yandex.ru>
2022-09-30 21:58:14 +0300

    +Test_EVG_on_branch_test_cash +---

--- Test_EVG_on_branch_test_cash.py
+++ Test_EVG_on_branch_test_cash.py
@@ -1,9 +1,9 @@
 def test_submit_user_details():
     app.given_opened_text_box_page()
 
-    browser.should(have.title('ToolsQA'))
-    browser.element('.main-header').should(have.text('Text Box'))
-    browser.element(by.text('Text Box')).should(be.visible)
+    # browser.should(have.title('ToolsQA'))
+    # browser.element('.main-header').should(have.text('Text Box'))
+    # browser.element(by.text('Text Box')).should(be.visible)
 
     browser.element('#userForm').all('input, textarea').should(have.size(4))
     browser.all('.form-label').element_by(have.text('Address')).should(have.text('Current Address'))
